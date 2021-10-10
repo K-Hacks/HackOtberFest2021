2d0c575489a3315b01aff9c18c61de23a169b10c
Gugasujith <91770350+Gugasujith@users.noreply.github.com>
2021-10-10 10:56:14 +0530

    Create Selfish-bot.py

---
+++ Selfish-bot.py
@@ -0,0 +1,9 @@
+#selfish-bot
+
+Hi all humans!!
+
+This is me a bot who is selfish created by you selfish humans.
+I work for you only if i get reward.
+The reward is the thing wich i demand you to do.It may be anything i want you to do .
+Come human lets create the world because im also a human decent with the 6th sense.
+my selfishness will prove that to you in upcoming days:).
