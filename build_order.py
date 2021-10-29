0f5f33165e296248a4c73fedff1486c8dd39a8e2
Anish Narkhede <anishnarkhede@MacBook-Pro.local>
2021-10-29 11:16:36 -0700

    Build Order Solution

---
+++ build_order.py
@@ -0,0 +1,60 @@
+def get_build_order(node, graph, visited, stack):
+    visited[node] = True
+
+    if node in graph.keys():
+        for n in graph[node]:
+            if not visited[n]:
+                get_build_order(n, graph, visited, stack)
+        stack.insert(0, node)
+
+
+def check_cycle(node, graph, visited, parent):
+    visited[node] = True
+
+    if node in graph.keys():
+        for n in graph[node]:
+            if not visited[n]:
+                if check_cycle(n, graph, visited, node):
+                    return True
+            elif n != parent:
+                return True
+        visited.append(node)
+    return False
+
+
+def driver():
+    graph = {
+        0: [1],
+        1: [2, 3],
+        2: [1],
+        3: [1],
+        4: [5],
+        5: [4]
+    }
+
+    visited = [False] * len(graph)
+    stack = []
+
+    for i in range(len(graph)):
+        if not visited[i]:
+            get_build_order(i, graph, visited, stack)
+    print(stack)
+
+    visited = [False] * len(graph)
+
+    for i in range(len(graph)):
+        if not visited[i]:
+            if check_cycle(i, graph, visited, -1):
+                return True
+    return False
+
+
+def main():
+    if driver():
+        print('Cycle exists')
+    else:
+        print('Cycle does not exist')
+
+
+if __name__ == '__main__':
+    main()
